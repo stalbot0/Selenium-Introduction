b7a8971dc0cbcc62631f2151060e098976a40c4d
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-07-31 18:08:29 -0500

    starting with the chrome driver

--- src2/main/java2/Main.java
+++ src2/main/java2/Main.java
@@ -1,5 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+//        using chrome driver to invoke the browser - ChromeDriver -> methods
+//        WebDriver interface that directs what the drivers can do
+        WebDriver driver = new ChromeDriver();
+
     }
 }
