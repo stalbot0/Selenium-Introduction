f90c1bd0e0fe1d5d8dced3e290f369b90009afe3
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-01 16:12:26 -0500

    introduced xpath and linktext

--- src2/main/java2/Locators.java
+++ src2/main/java2/Locators.java
@@ -20,10 +20,16 @@ public class Locators {
         driver.findElement(By.className("signInBtn")).click();
 
         System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
-//        if we do not have a tool such as selectors hub, we can use something like <$('p.error')> in the console itself
-//        to print all the elements that have that selector. If it could not be identified it will give you a
+//        ^^^ if we do not have a tool such as selectors hub, we can use something like <$('p.error')> in the console
+//        itself to print all the elements that have that selector. If it could not be identified it will give you a
 //        null response
-
-        driver.quit();
+        driver.findElement(By.linkText("Forgot your password?")).click();
+//        introducing Xpath
+//        ^^^ if we do not have a tool such as selectors hub, we can use something like
+//        <$x('//input[@placeholder="Name"]')> in the console itself to print all the elements that have that selector.
+//        If it could not be identified it will give you a null response
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Stephen");
+        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("email@gmail.com");
+        driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("555-555-5555");
     }
 }
