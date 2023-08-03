003a803d53540eaaadfc8c6370a741db3e5bfac3
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-03 16:39:23 -0500

    working with static dropdowns, but the webpage is not working for this udemy lecture. Getting 404 error due to updated webpage

---
+++ src2/main/java2/StaticDropdown.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+    public static void main(String[] args) {
+//        we don't need to set the system property because the new selenium handles it for us
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        WebElement staticDropdown = driver.findElement(By.id("ct100_mainContent_DropDownListCurrency"));
+//        select the class
+        Select dropdown = new Select(staticDropdown);
+//        the index order will not change for the static dropdowns (if the tag is select then it will be static)
+        dropdown.selectByIndex(3);
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+        dropdown.selectByVisibleText("AED");
+        System.out.println(dropdown.getFirstSelectedOption().getText());
+    }
+}
