506036182932529f4e13d4b0d1f79e58bc60737c
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-09 11:24:48 -0500

    added checkbox checking to UpdatedDropdown

--- src2/main/java2/UpdatedDropdown.java
+++ src2/main/java2/UpdatedDropdown.java
@@ -1,11 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class UpdatedDropdown {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.get("https://spicejet.com");
+        if (!driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected()) {
+//        anything that will match the id with the name that includes SeniorCitizenDiscount
+            driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+        }
+//        print the number of checkboxes present in the page
+        int checkboxCount = driver.findElements(By.cssSelector("input[type='checkbox']")).size();
+        System.out.println(checkboxCount); //should give us 6 but spicejet.com is a hoax
+
         driver.findElement(By.id("divpaxinfo")).click();
         try {
             Thread.sleep(1500);
