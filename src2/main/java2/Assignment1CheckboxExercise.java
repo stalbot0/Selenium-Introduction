544d8c3524c11e45d2ff0583921ea96fb745fb88
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-10 11:02:51 -0500

    completed checkbox exercise

---
+++ src2/main/java2/Assignment1CheckboxExercise.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class Assignment1CheckboxExercise {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("incognito");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        driver.findElement(By.id("checkBoxOption1")).click();
+        System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+
+        driver.findElement(By.id("checkBoxOption1")).click();
+        System.out.println(driver.findElement(By.id("checkBoxOption1")).isSelected());
+        Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+
+        int checkboxCountOnPage = driver.findElements(By.cssSelector("input[type='checkbox']")).size();
+        System.out.println(checkboxCountOnPage);
+        Assert.assertEquals(checkboxCountOnPage, 3);
+    }
+}
