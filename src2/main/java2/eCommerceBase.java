75904f080cab3bb0865aead81a25b0f937019851
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-14 16:59:34 -0500

    we don't want to use text when looking for dynamic options. So changed the xpath

--- src2/main/java2/eCommerceBase.java
+++ src2/main/java2/eCommerceBase.java
@@ -7,12 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class eCommerceBase {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        Thread.sleep(3000);
 //        let's add cucumber or broccoli
         String[] vegetablesToTest = {"Cucumber", "Brocolli", "Beetroot"};
         List<WebElement> vegetableOptions = driver.findElements(By.cssSelector("h4.product-name"));
+        int clickCount = 0;
 
         for (int i = 0; i < vegetableOptions.size(); i++) {
             List<String> newList = Arrays.asList(vegetablesToTest);
@@ -21,8 +23,13 @@ public class eCommerceBase {
             String formattedVeggieName = separatedVeggieName[0].trim();
 
             if (newList.contains(formattedVeggieName)) {
-                driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+                clickCount++;
+                if (clickCount == 3) {
+                    break;
+                }
             }
+
         }
     }
 }
