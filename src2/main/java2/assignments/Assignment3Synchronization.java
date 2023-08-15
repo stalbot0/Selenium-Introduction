488041dddac1299c81c61b819a0228792fc800a9
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-15 14:40:42 -0500

    changed structure of code base - added packages. Added the fluent wait lecture example

--- src2/main/java2/assignments/Assignment3Synchronization.java
+++ src2/main/java2/assignments/Assignment3Synchronization.java
@@ -1,4 +1,4 @@
-package Assignments;
+package assignments;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src2/main/java2/dropdowns/StaticDropdown.java
+++ src2/main/java2/dropdowns/StaticDropdown.java
@@ -1,3 +1,5 @@
+package dropdowns;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
