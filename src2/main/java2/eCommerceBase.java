22fd8688f94b4830ab26e8801a4fba94acfd403c
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-15 13:26:40 -0500

    added implicit and explicit wait to eCommerceBase but stuck with explicit wait following the lecture

--- src2/main/java2/eCommerceBase.java
+++ src2/main/java2/eCommerceBase.java
@@ -2,7 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,9 +13,29 @@ public class eCommerceBase {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-        Thread.sleep(3000);
+//        Thread.sleep(3000); //got rid of this because we now wait implicitly
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4)); //implicit wait applied globally
+//        we commented out the implicit wait because now it is not glocal as we have applied the explicit wait
+//        for the two steps that we needed to wait for
+        WebDriverWait wt = new WebDriverWait(driver, Duration.ofSeconds(5)); // explicit wait for this object max 5 sec
 //        let's add cucumber or broccoli
         String[] vegetablesToTest = {"Cucumber", "Brocolli", "Beetroot", "Carrot"};
+        addItems(driver, vegetablesToTest);
+        driver.findElement(By.cssSelector("a.cart-icon img[alt='Cart']")).click();
+//        proceed to checkout
+        driver.findElement(By.cssSelector("div.action-block button")).click();
+//        we need to wait for the page to load here
+        wt.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+        driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.cssSelector("button.promoBtn")).click();
+//        explicitly wait for the coupon to be applied
+        wt.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+//        then print out the message for the coupon
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+
+    }
+
+    public static void addItems(WebDriver driver, String[] vegetablesToTest) {
         List<WebElement> vegetableOptions = driver.findElements(By.cssSelector("h4.product-name"));
         int clickCount = 0;
 
@@ -29,7 +52,6 @@ public class eCommerceBase {
                     break;
                 }
             }
-
         }
     }
 }
