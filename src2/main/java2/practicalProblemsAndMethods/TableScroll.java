a5ec88afa5d12cc7be0b597b96e790f745bfa297
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-18 10:35:36 -0500

    added all table values together

--- src2/main/java2/practicalProblemsAndMethods/TableScroll.java
+++ src2/main/java2/practicalProblemsAndMethods/TableScroll.java
@@ -1,8 +1,12 @@
 package practicalProblemsAndMethods;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class TableScroll {
     public static void main(String[] args) throws InterruptedException {
@@ -14,5 +18,17 @@ public class TableScroll {
         Thread.sleep(3000);
 //        apply scroll to the table now
         js.executeScript("document.querySelector(\"div.tableFixHead\").scrollTop=5000;");
+//        div.tableFixHead table#product tbody tr td:nth-child(4)
+        List<WebElement> tableAmounts = driver.findElements(
+                By.cssSelector("div.tableFixHead table#product tbody tr td:nth-child(4)")
+        );
+
+        int sumOfTableAmounts = 0;
+
+        for (WebElement tableAmount : tableAmounts) {
+            sumOfTableAmounts += Integer.parseInt(tableAmount.getText());
+        }
+
+        System.out.println(sumOfTableAmounts);
     }
 }
