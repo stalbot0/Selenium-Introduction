3566549524ff6728c68324597acf7549df390a86
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-22 11:21:43 -0500

    assertions added when going through all links

--- src2/main/java2/miscTopics/BrokenLinks.java
+++ src2/main/java2/miscTopics/BrokenLinks.java
@@ -2,34 +2,52 @@ package miscTopics;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class BrokenLinks {
     public static void main(String[] args) throws IOException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 //        get one link using css selector
-        String soapUIURL = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+        WebElement soapUIURL = driver.findElement(By.cssSelector("a[href*='soapui']"));
 //        we don't have to actually click the link to see if it is broken or not
-//        write a script that will go through all links in the footer and scan to see which are broken
-//        we can get the status codes for navigation, if status code >= 400, the links are broken
-        HttpURLConnection connecktshun = (HttpURLConnection) new URL(soapUIURL).openConnection();
-        connecktshun.setRequestMethod("HEAD");
-        connecktshun.connect();
-        int respCode = connecktshun.getResponseCode();
+//        we can get the status codes for navigation, if status code >= 400, the link is broken
+//        this link will work
+        int respCode = connectionMethod(soapUIURL);
         System.out.printf("Response code for soapUI link: %d\n", respCode);
 
-        String brokenLinkURL = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
-        HttpURLConnection connecktshun2 = (HttpURLConnection) new URL(brokenLinkURL).openConnection();
-        connecktshun2.setRequestMethod("HEAD");
-        connecktshun2.connect();
-        int respCode2 = connecktshun2.getResponseCode();
+//        this link should be broken
+        WebElement brokenLink = driver.findElement(By.cssSelector("a[href*='brokenlink']"));
+        int respCode2 = connectionMethod(brokenLink);
         System.out.printf("Response code for broken link: %d\n", respCode2);
+
+        SoftAssert softAssert = new SoftAssert();
+//        write a script that will go through all links in the footer and scan to see which are broken
+        List<WebElement> footerLinks = driver.findElements(By.cssSelector("li.gf-li a"));
+        for (WebElement w : footerLinks) {
+            String msg = String.format("Broken link: %s, Response Code: %d\n", w.getText(), connectionMethod(w));
+//                soft assertion so that we can continue to run through all of them and check (see top)
+            softAssert.assertFalse(connectionMethod(w) >= 400, msg);
+        }
+//        when we assert all, we get beautiful messages in the output
+        softAssert.assertAll();
+    }
+
+    public static int connectionMethod(WebElement w) throws IOException {
+        String url = w.getAttribute("href");
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("HEAD");
+        connection.connect();
+        return connection.getResponseCode();
     }
 }
