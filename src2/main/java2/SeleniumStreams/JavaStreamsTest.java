234d4d3bb4bf180a2151e4770ce34116544ff541
Stephen Talbot <stephen.clark.talbot@gmail.com>
2023-08-23 19:11:56 -0500

    lambda for each with streams

--- src2/main/java2/SeleniumStreams/JavaStreamsTest.java
+++ src2/main/java2/SeleniumStreams/JavaStreamsTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class JavaStreamsTest {
     @Test
@@ -20,12 +21,20 @@ public class JavaStreamsTest {
         System.out.println(count);
     }
 
-//    lambda expression with stream to condense the code into something much cleaner
+    //    lambda expression with stream to condense the code into something much cleaner
     @Test
     public void streamFilter() {
         ArrayList<String> names = createFakeArrListOfNames();
+//        this will not modify the original stream
         long count = names.stream().filter(s -> s.startsWith("A")).count();
         System.out.println(count);
+
+//        long count2 = Stream.of("Alice", "Bob", "Susie", "Adam", "Stephanie").filter(s -> {
+//            s.startsWith("A");
+//            return false;
+//        }).count();
+//        print all of the names in the array list
+        names.stream().filter(s -> s.length() > 4).forEach(System.out::println);
     }
 
     public static ArrayList<String> createFakeArrListOfNames() {
